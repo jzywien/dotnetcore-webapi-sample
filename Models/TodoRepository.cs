fa0d72f43abeef87c266bb789e41afdf0f81f5ee
jzywien <jzywien@gmail.com>
2017-05-24 09:26:25 -0400

    Initial commit - Simple EF WebApi Sample

---
+++ Models/TodoRepository.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TodoApi.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(long key);
+        void Remove(long key);
+        void Update(TodoItem item);
+    }
+    public class TodoRepository : ITodoRepository
+    {
+        private readonly TodoContext _context;
+
+        public TodoRepository(TodoContext context)
+        {
+            _context = context;
+
+            if( _context.TodoItems.Count() == 0)
+                Add(new TodoItem { Name = "Item1" });
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        public void Add(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+        }
+
+        public TodoItem Find(long key)
+        {
+            return _context.TodoItems.FirstOrDefault(t => t.Key == key);
+        }
+
+        public void Remove(long key)
+        {
+            var entity = _context.TodoItems.First(t => t.Key == key);
+            _context.TodoItems.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TodoItem item)
+        {
+            _context.TodoItems.Update(item);
+            _context.SaveChanges();
+        }
+    }
+}
